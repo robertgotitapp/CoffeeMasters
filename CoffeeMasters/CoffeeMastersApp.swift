707ea50f23089c323277705dd3afc1b1a81ec8a0
Robert Nguyen <robert@gotitapp.co>
2022-07-12 15:58:46 +0700

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Robert Nguyen on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMasters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeMasters
+//
+//  Created by Robert Nguyen on 12/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
